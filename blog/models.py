20f1bf4b808a1c60230c3031680270d95e5bb83f
Sziráki Tibor <szikar@protonmail.com>
2017-05-08 21:22:35 +0200

    BLOGGER-3 Hozzászólás javítása
    Bug kijavítva. A Comment osztályból az "approved_comments" metódus áthelyezve a Post osztályba.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -9,13 +10,14 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date =timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
@@ -28,8 +30,8 @@ class Comment(models.Model):
         self.approved_comment = True
         self.save()
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
+    # def approved_comments(self):
+    #     return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.text
